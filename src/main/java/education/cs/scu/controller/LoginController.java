69b3d0f64d6950312d0d72feb5ed8b8d6ea6a005
mai ciu s <maicius@outlook.com>
2017-06-13 21:28:24 +0800

    修改返回类型，返回json文件

--- src/main/java/education/cs/scu/controller/LoginController.java
+++ src/main/java/education/cs/scu/controller/LoginController.java
@@ -3,10 +3,7 @@ package education.cs.scu.controller;
 import education.cs.scu.entity.User;
 import education.cs.scu.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,31 +11,31 @@ import javax.servlet.http.HttpSession;
 /**
  * Created by maicius on 2017/3/31.
  */
+@CrossOrigin
 @RestController
 public class LoginController {
 
     @Autowired
     LoginService loginService;
 
-    @RequestMapping(value="/userLogin")
-    public ModelAndView Test(HttpServletRequest request,
-                             @RequestParam(value="userName") String userName,
+    @RequestMapping(value="/userLogin", method= RequestMethod.POST)
+    public User userLogin(HttpServletRequest request,
+                             @RequestParam(value="username") String userName,
                              @RequestParam(value="password") String password) throws Exception{
         User user = new User(userName, password);
         System.out.println(userName);
-        ModelAndView mv = new ModelAndView();
         User loginUser = loginService.doUserLogin(user);
         HttpSession session = request.getSession();
         if(loginUser != null) {
             session.setAttribute("user", loginUser);
+            System.out.println("finish:" + loginUser.getNickName());
+            return loginUser;
         }else{
             User wrongUser = new User();
             wrongUser.setNickName("该用户不存在");
             session.setAttribute("user", wrongUser);
+            return null;
         }
-        mv.setViewName("login");
-        System.out.println("Controller finished");
-        return mv;
     }
 
 }
